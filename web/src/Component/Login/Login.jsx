083026c9245c8ab9ae1ec73dcb27f96855fee066
Bhaskar <bhaskar@dev.com>
2022-08-30 01:51:26 +0530

    2022.08.30

--- web/src/Component/Login/Login.jsx
+++ web/src/Component/Login/Login.jsx
@@ -1,62 +1,59 @@
 import { Link } from "react-router-dom";
 
 function LoginPage() {
+	return (
+		<>
+			<div id="titlebar" className="gradient">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h2>Log In</h2>
 
-    return (
-        <>
-            <div id="titlebar" className="gradient">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
+							{/* Breadcrumbs */}
+							<nav id="breadcrumbs" className="dark">
+								<ul>
+									<li>Log In</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-xl-5 offset-xl-3">
+						<div className="login-register-page">
+							{/* Welcome Text */}
+							<div className="welcome-text">
+								<h3>We're glad to see you again!</h3>
+								<span>
+									Don't have an account? <Link to="/register">Sign Up!</Link>
+								</span>
+							</div>
 
-                            <h2>Log In</h2>
+							{/* Form */}
+							<form method="post" id="login-form">
+								<div className="input-with-icon-left">
+									<i className="icon-material-baseline-mail-outline"></i>
+									<input type="text" className="input-text with-border" name="emailaddress" id="emailaddress" placeholder="Email Address" required />
+								</div>
 
-                            {/* Breadcrumbs */}
-                            <nav id="breadcrumbs" className="dark">
-                                <ul>
-                                    <li>Log In</li>
-                                </ul>
-                            </nav>
+								<div className="input-with-icon-left">
+									<i className="icon-material-outline-lock"></i>
+									<input type="password" className="input-text with-border" name="password" id="password" placeholder="Password" required />
+								</div>
 
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-5 offset-xl-3">
-
-
-                        <div className="login-register-page">
-                            {/* Welcome Text */}
-                            <div className="welcome-text">
-                                <h3>We're glad to see you again!</h3>
-                                <span>Don't have an account? <Link to="/register">Sign Up!</Link></span>
-                            </div>
-
-                            {/* Form */}
-                            <form method="post" id="login-form">
-                                <div className="input-with-icon-left">
-                                    <i className="icon-material-baseline-mail-outline"></i>
-                                    <input type="text" className="input-text with-border" name="emailaddress" id="emailaddress" placeholder="Email Address" required />
-                                </div>
-
-                                <div className="input-with-icon-left">
-                                    <i className="icon-material-outline-lock"></i>
-                                    <input type="password" className="input-text with-border" name="password" id="password" placeholder="Password" required />
-                                </div>
-
-                                {/* Button */}
-                                <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form">Log In <i className="icon-material-outline-arrow-right-alt"></i></button>
-                            </form>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-
+								{/* Button */}
+								<button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form">
+									Log In <i className="icon-material-outline-arrow-right-alt"></i>
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default LoginPage;
--- web/src/Component/Register/Register.jsx
+++ web/src/Component/Register/Register.jsx
@@ -1,100 +1,78 @@
-import { Component } from 'react';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
+function RegisterPage() {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ mode: "onInput" });
 
-class RegisterPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            'emailaddress': '',
-            'password': '',
-            're_password': '',
-        };
-        this.handleInputChange = this.handleInputChange.bind(this);
+	const onFormSubmit = (data) => console.log(data);
 
-    }
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        const name = target.name;
+	useEffect(() => {
+		console.log(errors.EmailAddress);
+	}, [errors]); // <- add the count variable here
 
-        this.setState({
-            [name]: value
-        });
-    }
-    render() {
-        return (
-            <>
-                <div id="titlebar" className="gradient">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-md-12">
+	return (
+		<>
+			<div id="titlebar" className="gradient">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<h2>Register</h2>
 
-                                <h2>Register</h2>
+							{/* Breadcrumbs */}
+							<nav id="breadcrumbs" className="dark">
+								<ul>
+									<li>Register</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-xl-5 offset-xl-3">
+						<div className="login-register-page">
+							{/* Welcome Text */}
+							<div className="welcome-text">
+								<h3 style={{ fontSize: "26px" }}>Let's create your account!</h3>
+								<span>
+									Already have an account? <Link to="/login">Log In!</Link>
+								</span>
+							</div>
 
-                                {/* Breadcrumbs */}
-                                <nav id="breadcrumbs" className="dark">
-                                    <ul>
-                                        <li>Register</li>
-                                    </ul>
-                                </nav>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-5 offset-xl-3">
-
-                            <div className="login-register-page">
-                                {/* Welcome Text */}
-                                <div className="welcome-text">
-                                    <h3 style={{ fontSize: '26px' }}>Let's create your account!</h3>
-                                    <span>Already have an account? <Link to='/login'>Log In!</Link></span>
-                                </div>
-
-                                {/* Form */}
-                                <form method="post" id="register-account-form" onSubmit={this.handleInputChange}>
-                                    <div className="input-with-icon-left">
-                                        <i className="icon-material-baseline-mail-outline"></i>
-                                        <input type="text" className="input-text with-border" name="emailaddress"
-                                            id="emailaddress-register" placeholder="Email Address" required
-                                            value={this.state.emailaddress}
-                                            onChange={this.handleInputChange} />
-                                    </div>
-
-                                    <div className="input-with-icon-left" title="Should be at least 8 characters long"
-                                        data-tippy-placement="bottom">
-                                        <i className="icon-material-outline-lock"></i>
-                                        <input type="password" className="input-text with-border" name="password"
-                                            id="password-register" placeholder="Password" required
-                                            value={this.state.password}
-                                            onChange={this.handleInputChange} />
-                                    </div>
-
-                                    <div className="input-with-icon-left">
-                                        <i className="icon-material-outline-lock"></i>
-                                        <input type="password" className="input-text with-border" name="re_password"
-                                            id="password-repeat-register" placeholder="Repeat Password" required
-
-                                            value={this.state.re_password}
-                                            onChange={this.handleInputChange} />
-                                    </div>
-
-                                    {/* Button */}
-                                    <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit"
-                                        form="login-form">Register <i className="icon-material-outline-arrow-right-alt"></i></button>
-
-                                </form>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
+							{/* Form */}
+							<form method="post" id="register-account-form" onSubmit={handleSubmit(onFormSubmit)}>
+								<div className="input-with-icon-left">
+									<i className="icon-material-outline-account-circle"></i>
+									<input type="text" className="input-text with-border" placeholder="Name" {...register("Name", { required: true })} />
+								</div>
+								<div className="input-with-icon-left">
+									<i className="icon-material-baseline-mail-outline"></i>
+									<input type="text" className={`input-text with-border ${errors.EmailAddress ? "valid" : "error"}`} placeholder="Email Address" {...register("EmailAddress", { required: true, pattern: /^\S+@\S+$/i })} />
+								</div>
+								<div className="input-with-icon-left" title="Should be at least 8 characters long" data-tippy-placement="bottom">
+									<i className="icon-material-outline-lock"></i>
+									<input type="password" className="input-text with-border" placeholder="Password" {...register("Password", { required: true, min: 8 })} />
+								</div>
+								<div className="input-with-icon-left">
+									<i className="icon-material-outline-lock"></i>
+									<input type="password" className="input-text with-border" placeholder="Repeat Password" {...register("RepeatPassword", { required: true, min: 8 })} />
+								</div>
+								{/* Button */}
+								<button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="register-account-form">
+									Register <i className="icon-material-outline-arrow-right-alt"></i>
+								</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
-
 export default RegisterPage;
